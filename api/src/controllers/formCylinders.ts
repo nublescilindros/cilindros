11680a80eb5b87f188166d1f2c41d1d51dd4adf5
htttw <138150719+htttw@users.noreply.github.com>
2023-11-16 15:24:05 -0400

    update:state cylinders and generate pdf

--- api/src/controllers/formCylinders.ts
+++ api/src/controllers/formCylinders.ts
@@ -204,6 +204,7 @@ const generatePdfCylinderCompany = async (req: any, res: any) => {
 
         const browser = await puppeteer.launch({
             headless: 'new',
+            executablePath: '/ruta/a/chromium',
         });
         const page = await browser.newPage();
         await page.setContent(html);
